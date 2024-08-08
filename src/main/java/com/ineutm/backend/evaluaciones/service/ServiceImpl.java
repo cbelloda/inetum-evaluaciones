283f823bdbb7a1b08e85035e97322bd527116355
carlos-belloda_pacifico <carlos.belloda@inetum.com>
2024-08-08 02:49:15 -0500

    join service and ontroller

--- src/main/java/com/ineutm/backend/evaluaciones/service/ServiceImpl.java
+++ src/main/java/com/ineutm/backend/evaluaciones/service/ServiceImpl.java
@@ -1,12 +1,13 @@
 package com.ineutm.backend.evaluaciones.service;
 
 import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
 
 @Service
 public class ServiceImpl {
 
-  public String getAccount(String userIdentity) {
-    return "el numero "+userIdentity;
+  public Flux<String> getAccount(String userIdentity) {
+    return Flux.range(1, 220).map(number -> "el numero "+userIdentity+"  "+number);
   }
   
 }
